c985552565423771d3fc8f81330abade13d43a82
Ivan <ivan_chetverin@mail.ru>
2026-04-20 16:25:19 +0300

    deployment structure

--- frontend/src/pages/objects/ui/Objects.jsx
+++ frontend/src/pages/objects/ui/Objects.jsx
@@ -1,8 +1,9 @@
+import { Catalog } from "pages/catalog";
+
 export const Objects = () => {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Объекты</h1>
-      <p>Здесь будет контент главной страницы</p>
+      <Catalog />
     </div>
   );
 };
