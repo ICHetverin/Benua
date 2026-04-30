dabb8ff59dfbe4b9abba91b06121d9a162ae5fa4
Ivan <ivan_chetverin@mail.ru>
2026-04-30 10:55:49 +0300

    fix: front api

--- frontend/src/shared/api/benuaApi.js
+++ frontend/src/shared/api/benuaApi.js
@@ -10,13 +10,13 @@ api.interceptors.response.use(
   (error) => Promise.reject(error?.response?.data ?? error),
 );
 
-export const getPersons = () => api.get('/api/persons');
-export const getPersonById = (id) => api.get(`/api/persons/${id}`);
+export const getPersons = () => api.get('/persons');
+export const getPersonById = (id) => api.get(`/persons/${id}`);
 
-export const getObjects = () => api.get('/api/objects');
-export const getObjectById = (id) => api.get(`/api/objects/${id}`);
+export const getObjects = () => api.get('/objects');
+export const getObjectById = (id) => api.get(`/objects/${id}`);
 
-export const createPerson = (data) => api.post('/api/persons', data);
-export const createObject = (data) => api.post('/api/objects', data);
+export const createPerson = (data) => api.post('/persons', data);
+export const createObject = (data) => api.post('/objects', data);
 
-export const search = (query) => api.get('/api/search', { params: { q: query } });
+export const search = (query) => api.get('/search', { params: { q: query } });
